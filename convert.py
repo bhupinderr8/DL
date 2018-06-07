99ad2c00339e6f76c92b7614706caefa3c5bca84
bhupinderr8 <bhupinder37kumar@gmail.com>
2018-06-07 14:09:03 +0530

    Changes commited

--- convert.py
+++ convert.py
@@ -11,18 +11,25 @@ new_height = 96
 
 # Manipulating Images
 X_data = []
+Y_data = []
 for i in range(9344):
-    current_image = Image.open('images/' + test['anno']['files'][0][0][0][i][0])
-    cropped_image = current_image.crop((test['anno']['objbbs'][0][0][i][0],test['anno']['objbbs'][0][0][i][1],test['anno']['objbbs'][0][0][i][2],test['anno']['objbbs'][0][0][i][3]))
-    resized_image = cropped_image.resize((new_width, new_height), Image.ANTIALIAS)
-    image_array = np.asarray(resized_image)
-    X_data.append(image_array)
+    if test['anno']['y'][0][0][1][i] == 1:
+        current_image = Image.open('images/' + test['anno']['files'][0][0][0][i][0])
+        cropped_image = current_image.crop((test['anno']['objbbs'][0][0][i][0],test['anno']['objbbs'][0][0][i][1],test['anno']['objbbs'][0][0][i][2],test['anno']['objbbs'][0][0][i][3]))
+        resized_image = cropped_image.resize((new_width, new_height), Image.ANTIALIAS)
+        image_array = np.asarray(resized_image)
+        X_data.append(image_array)
+        Y_data.append(test['anno']['y'][0][0][0][i])
 
 # Saving image data into X_data
 np.save('X_data.npy',X_data)
 
 
-# Loading and saving output data
-Y_array = np.asarray(test['anno']['y'][0][0])
-Y_final = np.rot90(Y_array)
-np.save('Y_data.npy', Y_final)
+# # Loading and saving output data
+# Y_array = np.asarray(test['anno']['y'][0][0])
+# Y_final = np.rot90(Y_array)
+# np.save('Y_data.npy', Y_final)
+#
+
+Y = np.asarray(Y_data)
+np.save('Y_data.npy', Y)
--- load.py
+++ load.py
@@ -1,6 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+
 
 X_data = np.load('X_data.npy')
 Y_data = np.load('Y_data.npy')
-
-print (X_data.shape)
+count = 0
+print (Y_data)
